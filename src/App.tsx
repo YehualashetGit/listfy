6800ec99d9db7f5f03db783aa6ebdf75485c9a42
yehualashet <yehualashetab@gmail.com>
2025-02-17 11:49:42 +0100

    Remove HomePage and set ProductsPage as default route
    - Deleted HomePage component
    - Updated App routing to use ProductsPage as the index route
    - Simplified routing configuration

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,13 @@
 import { Routes, Route } from 'react-router-dom'
 import { Layout } from '@/components/Layout'
 import { ProductsPage } from '@/pages/ProductsPage'
-import {HomePage} from '@/pages/HomePage'
-import {CartPage} from '@/pages/CartPage'
+import { CartPage } from '@/pages/CartPage'
 
 export default function App() {
   return (
     <Routes>
       <Route path="/" element={<Layout />}>
-        <Route index element={<HomePage />} />
+        <Route index element={<ProductsPage />} />
         <Route path="products" element={<ProductsPage />} />
         <Route path="cart" element={<CartPage />} />
       </Route>
